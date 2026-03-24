9bcb800443b8726a765b6459be9c722c94931a1d
coco-projects <coco-project@protonmail.com>
2026-03-24 16:34:36 +0800

    update

--- examples/demo2.php
+++ examples/demo2.php
@@ -4,5 +4,8 @@
 
 //    $db->removeTable('test1');
 
-    $db->createAllTable(true);
+//    $db->createAllTable(true);
 //    $db->dropAllTable();
+
+//    print_r($db->getAllTableStatus());
+    var_dump($db->isAllTablesExists());
